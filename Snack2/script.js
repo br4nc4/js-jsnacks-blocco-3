b8b6cb338efcda259044d16707ddcf7f1af2159a
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-25 17:03:33 +0200

    snack2 pt.2 working

--- Snack2/script.js
+++ Snack2/script.js
@@ -4,8 +4,15 @@
 //es. input = 4 allora 4 array generati
 //ogni array ha 10 numeri
 //i numeri sono generati casualmente
-let inputContainer =[];
+const n = parseInt(prompt("inserisci il numero di array"));
 
-for (let i = 0; i<inputContainer.length; i++){
-    
+
+for (let i = 0; i < n; i++){
+    let array = [];
+    for (let i=0; i<10; i++){
+        array.push(Math.floor(Math.random()*100)+1);
+    }
+    console.log(array)
 }
+
+
