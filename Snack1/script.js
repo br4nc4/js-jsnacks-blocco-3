63ea092cd32adfdb054a2ea1c2864034d67e5085
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-25 15:33:58 +0200

    test snack1 pt.1

---
+++ Snack1/script.js
@@ -0,0 +1,15 @@
+const containerEmpty = [];
+//sommma = somma + Input
+//somma < 50
+let somma = 0;
+
+do{
+    const input = parseInt(prompt("inserisci un numero"));
+    somma = somma + input;
+    containerEmpty.push(input);
+    if (somma > 50){
+        alert("la somma supera il valore di 50");
+    }
+} while (somma < 50)
+
+alert(somma)
